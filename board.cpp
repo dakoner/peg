a081b79f613832c3a6c989d806c280aa3f1c828a
David Konerding <dakoner@gmail.comwq>
2024-07-26 17:30:50 -0700

    Finish up C++ implementation

--- board.cpp
+++ board.cpp
@@ -1,18 +1,190 @@
 #include "board.h"
 #include <vector>
-int main(void) {
+#include <optional>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+Board::Board()
+{
+}
+Board::Board(const Board &board)
+{
+    std::copy(&board.board_[0][0], &board.board_[0][0] + nx * ny, &board_[0][0]);
+}
+const int Board::board_at(int i, int j)
+{
+    return board_[i][j];
+}
+
+int Board::score()
+{
+    int s = 0;
+    for (auto i = 0; i < nx; i++)
+    {
+        for (auto j = 0; j < ny; j++)
+        {
+            if (board_[i][j] == 1)
+                s++;
+        }
+    }
+    if (s == 1 && board_[3][3] == 1)
+        s = 0;
+
+    return s;
+}
+
+moves_t Board::possible_moves()
+{
+    moves_t moves;
+    for (auto x = 0; x < nx; x++)
+    {
+        for (auto y = 0; y < ny; y++)
+        {
+            if (board_[x][y] == 1)
+            {
+                auto peg_moves = moves_for_peg(x, y);
+                for (auto peg_move : peg_moves)
+                    moves.push_back(std::make_pair(std::make_pair(x, y), peg_move));
+            }
+        }
+    }
+    return moves;
+}
+
+peg_moves_t Board::moves_for_peg(int x, int y)
+{
+    peg_moves_t peg_moves;
+
+    peg_moves_t move_offsets = {{-2, 0}, {2, 0}, {0, -2}, {0, 2}};
+    for (auto dxdy : move_offsets)
+    {
+        int new_x = x + dxdy.first;
+        int new_y = y + dxdy.second;
+        if (0 <= new_x && new_x <= 6 && 0 <= new_y && new_y <= 6)
+        {
+            if (board_[new_x][new_y] == 0)
+            {
+                if (board_[(x + new_x) / 2][(y + new_y) / 2] == 1)
+                {
+                    peg_moves.push_back(position_t(new_x, new_y));
+                }
+            }
+        }
+    }
+    return peg_moves;
+}
+
+void Board::move(move_t move)
+{
+    auto fxy = move.first;
+    auto txy = move.second;
+    auto from_x = fxy.first;
+    auto from_y = fxy.second;
+    auto to_x = txy.first;
+    auto to_y = txy.second;
+
+    board_[from_x][from_y] = 0;
+    board_[to_x][to_y] = 1;
+    board_[(from_x + to_x) / 2][(from_y + to_y) / 2] = 0;
+}
+
+std::ostream &operator<<(std::ostream &out, const Board& board)
+{
+    for (auto i = 0; i < nx; i++)
+    {
+        for (auto j = 0; j < ny; j++)
+        {
+            std::cout << board.board_[i][j];
+        }
+        std::cout << std::endl;
+    }
+    return std::cout;
+}
+
+bool Board::operator==(const Board board) const
+{
+    for (int i = 0; i < nx; i++)
+    {
+        for (int j = 0; j < ny; j++)
+        {
+            if (board_[i][j] != board.board_[i][j])
+                return false;
+        }
+    }
+    return true;
+}
+
+Solver::Solver(Board board) : board_(board)
+{
+    stack.push(board);
+}
+
+moves_t Solver::build_solution()
+{
+    moves_t solution;
+    while (true)
+    {
+        if (!parent.contains(board_))
+        {
+            solution.reverse();
+            return solution;
+        }
+        auto move_and_board = parent[board_];
+        solution.push_back(move_and_board.first);
+        board_ = move_and_board.second;
+    }
+}
+
+moves_t Solver::solve_internal()
+{
+    board_ = stack.top();
+    stack.pop();
+    if (!boards_visited.contains(board_))
+    {
+        boards_visited.insert(board_);
+        auto moves = board_.possible_moves();
+        if (moves.size() == 0)
+        {
+            auto score = board_.score();
+            if (score == 0) {
+                return build_solution();
+            }
+        }
+        for (auto move : moves)
+        {
+            Board b(board_);
+            b.move(move);
+            auto p = make_pair(move, board_);
+            auto i = make_pair(b, p);
+            parent.insert(i);
+            stack.push(b);
+        }
+    }
+    return moves_t();
+}
+
+void Solver::solve()
+{
+    moves_t result;
+    while (true)
+    {
+        result = solve_internal();
+        if (result.size())
+            break;
+    }
+    for(auto move: result) {
+        std::cout << "Move: " << move.first.first << "," << move.first.second << " " << move.second.first << "," << move.second.second << std::endl;
+    }
+}
+
+int main(void)
+{
 
     Board b;
-    auto moves = b.possible_moves();
-    auto move = moves.front();
-    std::cout
-            << "(" << move.first.first << "," << move.first.second << ") "
-            <<  "(" <<move.second.first << "," << move.second.second << ")"
-            << std::endl;
-    std::cout << b << std::endl;
-    b.move(move);
-    std::cout << b << std::endl;
-    std::cout << b.board_at(0,0) << std::endl;
-    std::unordered_set<Board, BoardHash> boards_visited;
-
+    Solver s(b);
+    s.solve();
 }
--- board.h
+++ board.h
@@ -1,3 +1,4 @@
+#include <optional>
 #include <unordered_set>
 #include <unordered_map>
 #include <stack>
@@ -7,8 +8,14 @@
 const int nx = 7;
 const int ny = 7;
 
+typedef std::pair<int, int> position_t;
+typedef std::list<position_t> peg_moves_t;
+typedef std::pair<position_t, position_t> move_t;
+typedef std::list<move_t> moves_t;
+
 class Board
 {
+public:
     int board_[nx][ny] = {
         {2, 2, 1, 1, 1, 2, 2},
         {2, 2, 1, 1, 1, 2, 2},
@@ -19,169 +26,54 @@ class Board
         {2, 2, 1, 1, 1, 2, 2},
     };
 
-public:
-    Board()
-    {
-    }
-    Board(const Board &board)
-    {
-        std::copy(&board.board_[0][0], &board.board_[0][0] + nx * ny, &board_[0][0]);
-    }
-    const int board_at(int i, int j)
-    {
-        return board_[i][j];
-    }
-    int score()
-    {
-        int s = 0;
-        for (auto i = 0; i < nx; i++)
-        {
-            for (auto j = 0; j < ny; j++)
-            {
-                if (board_[i][j] == 1)
-                    s++;
-            }
-        }
-        if (s == 1 && board_[3][3] == 1)
-            s = 0;
+    Board();
+    Board(const Board &board);
 
-        return s;
-    }
+    const int board_at(int i, int j);
 
-    std::list<std::pair<std::pair<int, int>, std::pair<int, int>>> possible_moves()
+    int score();
+
+    moves_t possible_moves();
+
+    peg_moves_t moves_for_peg(int x, int y);
+
+    void move(move_t move);
+
+    friend std::ostream &operator<<(std::ostream &out, const Board& board);
+
+    bool operator==(const Board board) const;
+
+    struct BoardHash
     {
-        std::list<std::pair<std::pair<int, int>, std::pair<int, int>>> moves;
-        for (auto x = 0; x < nx; x++)
+        size_t operator()(Board board) const
         {
-            for (auto y = 0; y < ny; y++)
+            std::hash<int> hasher;
+            size_t seed = 0;
+            for (int i = 0; i < nx; i++)
             {
-                if (board_[x][y] == 1)
+                for (int j = 0; j < ny; j++)
                 {
-                    auto peg_moves = moves_for_peg(x, y);
-                    for (auto peg_move : peg_moves)
-                        moves.push_back(std::make_pair(std::make_pair(x, y), peg_move));
+                    seed ^= hasher(board.board_at(i, j) + 0x9e3779b9 + (seed << 6) + (seed >> 2));
                 }
             }
+            return seed;
         }
-        return moves;
-    }
-
-    std::list<std::pair<int, int>> moves_for_peg(int x, int y)
-    {
-        std::list<std::pair<int, int>> peg_moves;
-
-        std::list<std::pair<int, int>> move_offsets = {{-2, 0}, {2, 0}, {0, -2}, {0, 2}};
-        for (auto dxdy : move_offsets)
-        {
-            int new_x = x + dxdy.first;
-            int new_y = y + dxdy.second;
-            if (0 <= new_x && new_x <= 6 && 0 <= new_y && new_y <= 6)
-            {
-                if (board_[new_x][new_y] == 0)
-                {
-                    if (board_[(x + new_x) / 2][(y + new_y) / 2] == 1)
-                    {
-                        peg_moves.push_back(std::pair<int, int>(new_x, new_y));
-                    }
-                }
-            }
-        }
-        return peg_moves;
-    }
-
-    void move(std::pair<std::pair<int, int>, std::pair<int, int>> move)
-    {
-        auto fxy = move.first;
-        auto txy = move.second;
-        auto from_x = fxy.first;
-        auto from_y = fxy.second;
-        auto to_x = txy.first;
-        auto to_y = txy.second;
-
-        board_[from_x][from_y] = 0;
-        board_[to_x][to_y] = 1;
-        board_[(from_x + to_x) / 2][(from_y + to_y) / 2] = 0;
-    }
-
-    friend std::ostream &operator<<(std::ostream &out, Board const &board)
-    {
-        for (auto i = 0; i < nx; i++)
-        {
-            for (auto j = 0; j < ny; j++)
-            {
-                std::cout << board.board_[i][j];
-            }
-            std::cout << std::endl;
-        }
-        return std::cout;
-    }
-};
-
-
-struct BoardHash
-{
-    size_t operator()(Board& board) const
-    {
-        std::hash<int> hasher;
-        size_t seed = 0;
-        for (int i = 0; i < nx; i++)
-        {
-            for (int j = 0; j < ny; j++)
-            {
-                seed ^= hasher(board.board_at(i, j) + 0x9e3779b9 + (seed<<6) + (seed>>2));
-            }
-        }
-        return seed;
-    }
+    };
 };
 
 class Solver
 {
     Board board_;
-    std::stack<std::pair<Board, std::pair<std::pair<int, int>, std::pair<int, int>>>> stack;
-         std::unordered_set<Board> boards_visited;
-    //     std::unordered_map<Board, std::pair<std::pair<int, int>, std::pair<int, int>>, Board> parent;
+    std::stack<Board> stack;
+    std::unordered_set<Board, Board::BoardHash> boards_visited;
+    std::unordered_map<Board, std::pair<move_t, Board>, Board::BoardHash> parent;
 
-    // public:
-    //     Solver(Board board) : board_(board)
-    //     {
-    //     }
+public:
+    Solver(Board board);
 
-    //     std::list<std::pair<std::pair<int, int>, std::pair<int, int>>> build_solution()
-    //     {
-    //     }
+    moves_t build_solution();
 
-    //     std::list<std::pair<std::pair<int, int>, std::pair<int, int>>> solve_internal()
-    //     {
-    //         auto board_and_move = stack.top();
-    //         auto board = board_and_move.first;
-    //         stack.pop();
-    //         if (!boards_visited.contains(board))
-    //         {
-    //             boards_visited.insert(board);
-    //             auto moves = board.possible_moves();
-    //             if (moves.size() == 0)
-    //             {
-    //                 auto score = board.score();
-    //                 if (score == 0)
-    //                     return build_solution();
-    //             }
-    //             for (auto move : moves)
-    //             {
-    //                 Board b(board);
-    //                 b.move(move);
-    //                 parent.insert(std::make_pair(b, make_pair(move, board)));
-    //                 stack.push(std::make_pair(b, move));
-    //             }
-    //         }
-    //     }
+    moves_t solve_internal();
 
-    //     void solve()
-    //     {
-    //         while (true)
-    //         {
-    //             auto result = solve_internal();
-    //             std::cout << result;
-    //         }
-    //     }
+    void solve();
 };
